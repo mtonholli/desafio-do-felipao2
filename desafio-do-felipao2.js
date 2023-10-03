e9f255f7c2d148541987538e214ade46c1c2d90e
mtonholli <matheustonholli@hotmail.com>
2023-10-03 01:03:18 -0300

    Envio do segundo desafio do professor Felipe.

--- desafio-do-felipao2.js
+++ desafio-do-felipao2.js
@@ -2,13 +2,14 @@ let rankReturn = calculaRank(1800)
 console.log(`"O rank do personagem com esse número de vitórias é: ${rankReturn}"`)
 
 
-function calculaRank(vitorias){
-    if (vitorias < 10) {rank = "Ferro";
-	    }	else if (11 < vitorias && vitorias <= 20){rank = "Bronze";
-    	}	else if (21 < vitorias && vitorias <= 50){rank = "Prata";
-    	}	else if (51 < vitorias && vitorias <= 80){rank = "Ouro";
-    	}	else if (81 < vitorias && vitorias <= 90){rank = "Diamante";
-    	}   else if (91 < vitorias && vitorias <= 100){rank = "Lendário";
+function calculaRank(vitorias = 0, derrotas = 0){
+    let saldo = vitorias - derrotas
+    if (saldo < 10) {rank = "Ferro";
+	    }	else if (11 < saldo && saldo <= 20){rank = "Bronze";
+    	}	else if (21 < saldo && saldo <= 50){rank = "Prata";
+    	}	else if (51 < saldo && saldo <= 80){rank = "Ouro";
+    	}	else if (81 < saldo && saldo <= 90){rank = "Diamante";
+    	}   else if (91 < saldo && saldo <= 100){rank = "Lendário";
     	}	else {rank = "Imortal";
     } return rank
 }
